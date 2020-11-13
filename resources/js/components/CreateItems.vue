33f64c5d6abd7944eed3215ce81f05526f6bd92a
James-N-M <jamesm0025@gmail.com>
2020-11-12 19:09:57 -0500

    Fixed issue with adding items to an Ad

--- resources/js/components/CreateItems.vue
+++ resources/js/components/CreateItems.vue
@@ -5,22 +5,22 @@
             <h3>Add Items</h3>
             <div class="form-group">
                 <label>Item {{ counter + 1 }} name</label>
-                <input v-model="item.name" type="text" class="form-control" required>
+                <input :name="'items['  + counter  + '][name]'" v-model="item.name" type="text" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>Item {{ counter + 1 }} description</label>
-                <input v-model="item.description" type="text" class="form-control">
+                <input :name="'items['  + counter  + '][description]'" v-model="item.description" type="text" class="form-control">
             </div>
 
             <div class="form-group">
                 <label>Item {{ counter + 1 }} price</label>
-                <input v-model="item.price" type="text" class="form-control" required>
+                <input :name="'items['  + counter  + '][price]'" v-model="item.price" type="text" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>Item {{ counter + 1 }} category</label>
-                <select v-model="item.category" type="text" class="form-control">
+                <select :name="'items['  + counter  + '][category_id]'" v-model="item.category" type="text" class="form-control">
                     <option value="1">Apparel</option>
                     <option value="2">Electronics</option>
                     <option value="3">Entertainment</option>
